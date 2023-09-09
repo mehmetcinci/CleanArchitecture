3d09dc66fb16def692f66d5cecf196b201fddef8
Mehmet Cinci <mehmet_cinci@hotmail.com>
2023-09-09 23:27:57 +0300

    First commit.

---
+++ WebApp/Program.cs
@@ -0,0 +1,33 @@
+using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
